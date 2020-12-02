fde53225f612d3fcd126ca4482599b2feebf00dd
SaurabhSuri <saurabhsuri99@gmail.com>
2020-12-02 19:46:35 +0530

    Second Commit, add_product, cat and sub cat dropdown

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
+use Illuminate\Support\Facades\DB;
+use App\Category;
+use Auth;
+use Session;
+
 
 class ProductsController extends Controller
 {
-    //
+    public function addProducts(Request $request)
+    {
+    	$category=DB::table('categories')->where('parent_id',0)->get();
+    	$categories_dropdown="<option value=0 selected disabled>Select</option>";
+    	foreach($category as $cat)
+    	{
+    		$categories_dropdown.="<option value='".$cat->id."'>".$cat->name."</option>";
+    		$sub_categories=DB::table('categories')->where('parent_id',"=",$cat->id)->get();
+    			foreach($sub_categories as $sub_cat)
+    			{
+    				$categories_dropdown.="<option value='".$sub_cat->id."'>&nbsp;--&nbsp;".$sub_cat->name."</option>";
+    			}
+    	}
+    	//dump($categories_dropdown);die;
+    	return view('admin.products.add_product', compact('categories_dropdown'));    
+    }
 }
--- resources/views/layouts/admin_layout/admin_design.blade.php
+++ resources/views/layouts/admin_layout/admin_design.blade.php
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href={{asset("css/backend_css/bootstrap-responsive.min.css")}} />
 <link rel="stylesheet" href={{asset("css/backend_css/fullcalendar.css")}} />
 <link rel="stylesheet" href={{asset("css/backend_css/matrix-style.css")}} />
+<link rel="stylesheet" href={{asset("css/backend_css/uniform.css")}} />
 <link rel="stylesheet" href={{asset("css/backend_css/matrix-media.css")}} />
 <link href={{asset("fonts/backend_fonts/css/font-awesome.css")}} rel="stylesheet" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
